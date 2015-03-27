67f9d0b21780ee6ccb557e09061fc2d7f56dc0c6
Richard Musiol <mail@richard-musiol.de>
2015-03-27 18:28:34 +0100

    initial commit

---
+++ json2prom.go
@@ -0,0 +1,127 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+type source struct {
+	URL       string
+	Namespace string
+	Subsystem string
+	Labels    map[string]string
+	Keys      map[string]struct {
+		Skip           bool
+		MapValue       map[string]float64
+		UseKeysAsLabel string
+	}
+}
+
+func main() {
+	sourcesJSON := os.Getenv("SOURCES")
+	if sourcesJSON == "" {
+		log.Print("environemnt variable SOURCES is requred")
+		return
+	}
+
+	var sources []*source
+	if err := json.Unmarshal([]byte(sourcesJSON), &sources); err != nil {
+		log.Print(err)
+		return
+	}
+
+	for _, s := range sources {
+		prometheus.MustRegister(s)
+	}
+	http.Handle("/metrics", prometheus.Handler())
+
+	addr := os.Getenv("HTTP_ADDR")
+	if addr == "" {
+		addr = ":8080"
+	}
+	fmt.Println("listening on " + addr)
+	log.Print(http.ListenAndServe(addr, nil))
+}
+
+func (s *source) Describe(descs chan<- *prometheus.Desc) {
+	metrics := make(chan prometheus.Metric)
+	done := make(chan struct{})
+	go func() {
+		for m := range metrics {
+			descs <- m.Desc()
+		}
+		close(done)
+	}()
+	s.Collect(metrics)
+	close(metrics)
+	<-done
+}
+
+func (s *source) Collect(metrics chan<- prometheus.Metric) {
+	resp, err := http.Get(s.URL)
+	if err != nil {
+		log.Print(err)
+		return
+	}
+	defer resp.Body.Close()
+
+	var value interface{}
+	if err := json.NewDecoder(resp.Body).Decode(&value); err != nil {
+		log.Print(err)
+		return
+	}
+
+	var labelNames []string
+	var labelValues []string
+	for k, v := range s.Labels {
+		labelNames = append(labelNames, k)
+		labelValues = append(labelValues, v)
+	}
+	s.processValue(nil, labelNames, labelValues, value, metrics)
+}
+
+func (s *source) processValue(keys []string, labelNames, labelValues []string, value interface{}, metrics chan<- prometheus.Metric) {
+	switch value := value.(type) {
+	case map[string]interface{}:
+		for k2, v2 := range value {
+			d := s.Keys[k2]
+			if d.Skip {
+				continue
+			}
+			if d.MapValue != nil {
+				s.processValue(append(keys, strings.Trim(k2, "_")), labelNames, labelValues, d.MapValue[v2.(string)], metrics)
+				continue
+			}
+			if d.UseKeysAsLabel != "" {
+				for k3, v3 := range v2.(map[string]interface{}) {
+					s.processValue(append(keys, strings.Trim(k2, "_")), append(labelNames, d.UseKeysAsLabel), append(labelValues, k3), v3, metrics)
+				}
+				continue
+			}
+			s.processValue(append(keys, strings.Trim(k2, "_")), labelNames, labelValues, v2, metrics)
+		}
+	case float64:
+		if value == 0 {
+			return
+		}
+		labels := make(prometheus.Labels)
+		for i, name := range labelNames {
+			labels[name] = labelValues[i]
+		}
+		g := prometheus.NewGauge(prometheus.GaugeOpts{
+			Namespace:   s.Namespace,
+			Subsystem:   s.Subsystem,
+			Name:        strings.Join(keys, "_"),
+			Help:        strings.Join(keys, ".") + " of " + s.URL,
+			ConstLabels: labels,
+		})
+		g.Set(value)
+		metrics <- g
+	}
+}
